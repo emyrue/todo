b96649063e3375b3666fa6b044eeff807c9220a5
emyrue <94935633+emyrue@users.noreply.github.com>
2022-03-22 11:58:16 -0600

    Classes added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import ListItem from './listItem.js';
+import List from './list.js';
+
+const toDo = new List();
+const toDoItem1 = new ListItem('wash the dishes', false, 0);
+const toDoItem2 = new ListItem('complete To Do list project', false, 1);
+toDo.add(toDoItem1);
+toDo.add(toDoItem2);
