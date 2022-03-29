7c62d805bc403b2676cf3fe4844f749cf4d7c276
fasaldana <fernando_s_98@hotmail.com>
2022-03-29 16:23:53 -0500

    Added html test

--- src/createList.js
+++ src/createList.js
@@ -8,15 +8,12 @@ function display(index, toDo) {
 function createList(element, list) {
   element.innerHTML = '';
   list.sort();
-  let itemsAdded = 0;
   for (let i = 0; i < list.list.length; i += 1) {
     const newElement = document.createElement('li');
     newElement.className = 'list-item';
     newElement.innerHTML = display(i, list);
     element.appendChild(newElement);
-    itemsAdded += 1;
   }
-  return itemsAdded;
 }
 
 module.exports = createList;
--- src/list.test.js
+++ src/list.test.js
@@ -4,9 +4,23 @@
 
 const List = require('./list.js');
 const createList = require('./createList.js');
-
-const testList = new List();
+document.body.innerHTML = `
+<section class="to-do">
+<div class="heading">
+    <h1>Today's To Do</h1>
+</div>
+<form>
+    <div class="new-item">
+        <input class="new-list-item" type="text" placeholder="Add to your list">
+    </div>
+    <ul class="list"></ul>
+    <button class="clear" type="submit">Clear All Completed</button>
+  </form>
+</section>
+`;
 const testListElement = document.querySelector('.list');
+const testList = new List();
+jest.mock('./index');
 
 describe('add and delete', () => {
   test('add item localStorage', () => {
@@ -25,13 +39,22 @@ describe('add and delete', () => {
     testList.add({ description: 'Socks2', completed: false, index: 2 });
     testList.remove(0);
     testList.store();
-    expect(testList.checkStorage()).toEqual([{ description: 'Socks1', completed: false, index: 1 }, 
-    { description: 'Socks2', completed: false, index: 2 }]);
+    expect(testList.checkStorage()).toEqual([
+      { description: 'Socks1', completed: false, index: 1 },
+      { description: 'Socks2', completed: false, index: 2 },
+    ]);
   });
-  test('add item html', () => {
-    const createTestList = document.createElement('ul')
+  test('checking list elements', () => {
     testList.fixIndex();
-    // createList(createTestList, testList);
-    expect(createList(createTestList, testList)).toBe(2);
+    createList(testListElement, testList);
+    let todolist = document.querySelectorAll('.list-item');
+    expect(todolist.length).toBe(2);
+  });
+
+  test('Adding an item and checking HTML', () => {
+    testList.add({ description: 'Socks1', completed: false, index: 2 });
+    createList(testListElement, testList);
+    todolist = document.querySelectorAll('.list-item');
+    expect(todolist.length).toBe(3);
   });
 });
