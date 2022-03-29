6b4c3a5eb7fcb0c39eda522fb2891d6012244b08
fasaldana <fernando_s_98@hotmail.com>
2022-03-29 15:13:26 -0500

    Test running

--- src/list.test.js
+++ src/list.test.js
@@ -1,13 +1,26 @@
 /*
-* @jest-environment jsdom
-*/
+ * @jest-environment jsdom
+ */
 
 const List = require('./list.js');
 
 const testList = new List();
 
 describe('add and delete', () => {
-  test('check storage', () => {
-    expect(testList.checkStorage()).toBeNull();
+  test('add item', () => {
+    testList.add({ description: 'Socks', completed: false, index: 0 });
+    testList.store();
+    expect(testList.checkStorage()).toEqual([
+      {
+        description: 'Socks',
+        completed: false,
+        index: 0,
+      },
+    ]);
+  });
+  test('remove item', () => {
+    testList.remove(0);
+    testList.store();
+    expect(testList.checkStorage()).toEqual([]);
   });
 });
